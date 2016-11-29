a8d1be6b2b90e770cafbd028586ff7b75e499f74
Clayton Mentzer <clay@Claytons-MacBook-Pro.local>
2016-11-29 13:36:51 -0500

    added custom folder to tracking

---
+++ custom/aliases.zsh
@@ -0,0 +1,11 @@
+# Custom Aliases go in here
+
+alias ls='ls -G'
+alias ll='ls -alh'
+alias updatedb='sudo /usr/libexec/locate.updatedb'
+alias updatemotd='python /Users/clay/programming/motd_shit/quote/getQuote.py'
+
+alias dodo='ls'
+
+
+
